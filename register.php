860b51463015a0cd042a03d0cd1b453d3b34d63f
Sacit Küheylan <sacit.kuheylan@yandex.com>
2021-10-25 16:32:21 +0300

    Validation added to register

--- register.php
+++ register.php
@@ -10,10 +10,18 @@ $username = $_POST['Username'];
 $password = $_POST['Password'];
 $email = $_POST['Email'];
 
-$sql = "SELECT PhoneNumber FROM User WHERE PhoneNumber = '" . $phonenumber . "'";
+$sql = "SELECT * FROM User WHERE PhoneNumber = '" . $phonenumber . "'";
+$sql1 = "SELECT * FROM User WHERE Username = '" . $username . "'";
+$sql2 = "SELECT * FROM User WHERE Email = '" . $email . "'";
 
 $result = mysqli_query($db, $sql);
 $count = mysqli_num_rows($result);
+
+$result1 = mysqli_query($db, $sql1);
+$count1 = mysqli_num_rows($result1);
+
+$result2 = mysqli_query($db, $sql2);
+$count2 = mysqli_num_rows($result2);
 
 /*
 $sql2 = "SELECT DeviceId FROM Device WHERE DeviceIMEI = '" . $imei . "'";
@@ -31,14 +39,21 @@ if ($sql2count == 1) {
 */
 
 if ($count == 1) {
-	echo json_encode("Error");
-} else {
+	echo json_encode("PhoneNumRegistered");
+} 
+else if ($count1 == 1) {
+	echo json_encode("UsernameRegistered");
+} 
+else if ($count2 == 1) {
+	echo json_encode("EmailRegistered");
+} 
+else {
 	//$insert_dep = "INSERT INTO User(NameSurname,PhoneNumber,Username,Password,Email,DeviceId)VALUES('" . $name . "','" . $phonenumber . "',
 	//	'" . $username . "','" . $password . "','" . $email . "','" . $updateid . "')";
 	$insert = "INSERT INTO User(NameSurname,PhoneNumber,Username,Password,Email)VALUES('" . $name . "','" . $phonenumber . "',
 		'" . $username . "','" . $password . "','" . $email . "')";
 	$query = mysqli_query($db, $insert);
 	if ($query) {
-		echo json_encode(" Success ");
+		echo json_encode("Success");
 	}
 }
